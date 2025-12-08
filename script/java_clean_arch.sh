3e03d000529b9e14c257dd7f5b43c5a4ceac2981
Quang Hoang <32516654+HQhalo@users.noreply.github.com>
2025-12-08 15:10:52 +0700

    Add script to create clean architecture structure
    This script creates a directory structure for a clean architecture in a Java project.

---
+++ script/java_clean_arch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Create Clean Architecture Structure"
+
+mkdir app
+mkdir app/dto
+mkdir app/mapper
+mkdir app/usecase
+mkdir domain
+mkdir domain/model
+mkdir domain/repository
+mkdir domain/service
+mkdir infra
+mkdir infra/config
+mkdir infra/persistence
+mkdir infra/persistence/adapter
+mkdir infra/persistence/entity
+mkdir infra/persistence/mapper
+mkdir infra/persistence/repository
+mkdir infra/service
+mkdir present
+mkdir present/controller
+mkdir present/interceptor
+mkdir present/request
+mkdir present/response
